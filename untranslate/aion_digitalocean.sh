bdd42f0ff9aa00283ff4a269f1d03a56072f91d9
nainoia-inc <nainoia-inc@signedon.net>
2023-02-11 11:49:29 -0500

    Advertise TOR address on website

---
+++ untranslate/aion_digitalocean.sh
@@ -0,0 +1,10 @@
+echo "RSYNC AIONIAN BIBLE TO DIGITALCLOUD!"
+
+touch ../www-production-files/do-test
+# works
+#scp -P 22 -p -i ~/.ssh/id_new -o User=apache  ../www-production-files/do-test 68.183.200.96:/var/www/html/
+# rsync is better
+rsync -r -a -v --progress -e "ssh -i ~/.ssh/id_new -l apache  -p 22" ../www-production-files/ 68.183.200.96:/var/www/html/
+
+
+echo "DONE"
