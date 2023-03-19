4cee5e9cfba3f0bd7954de6c16d482c045870e11
nainoia-inc <nainoia-inc@signedon.net>
2023-03-18 22:48:06 -0400

    Routine Bible test updates.

--- untranslate/aion_3t_translator.php
+++ untranslate/aion_3t_translator.php
@@ -48,7 +48,7 @@ $HEADER = <<<EOT
 # Extended Strong's numerical indexes linked to lexicon entries.
 # Word morphhology codes linked to explanatory entries.
 #
-# STATEMENT
+# BELIEF:
 # 1. In one true God, existing eternally as one God in three persons: Father, Son, and Holy Spirit, John 14:9-21, who is in essence spirit, John 4:24, light, 1 John 1:5, and love, 1 John 4:8.
 # 2. That the 66 books of the Old and New Testaments are the unique, inerrant, inspired Word of God in the original autographs, and the final authority in all matters of faith and conduct, 2 Tim 3:16.
 # 3. In the sovereignty and active rule of God in creation, the fall, history, revelation, miracles, prophecy, redemption, and final judgment, Romans 8:20-21.
@@ -63,7 +63,22 @@ $HEADER = <<<EOT
 # 12. In the final resurrection of redeemed mankind to the enjoyment of God forever, and the damnation of those excluded from the Book of Life to the Lake of Fire prepared for the Devil and his angels for the ages of the ages, Matthew 25:31-46 and Revelation 20:10.
 # 13. This statement of Christian faith does not exhaust the extent of our beliefs. The Bible itself, as the inspired and infallible Word of God that speaks with final authority concerning truth, morality, and the proper conduct of mankind, is the sole and final source of all that we believe.
 #
-# TRANSLATING:
+# PRACTICE:
+# 1. To glorify God and enjoy him forever, Revelation 21:1-7.
+# 2. To defend that true religion is defined by the Christian Scriptures, with God alone as the infallible interpreter of his Word, 2 Timothy 3:14-16 and 1 John 2:20-27.
+# 3. To defend that true religion obeys Christ’s commands and imitates his character, John 8:31-23 and 1 John 2:3-6.
+# 4. To love and receive all those who trust Jesus as members of the family of Christ and wholly participate in regular Christian fellowship, Romans 10:9 and Hebrews 10:25.
+# 5. To honor marriage as ordained by God and to acknowledge God’s only design as the lifetime covenant relationship between one man and one woman, an illustration of Christ and the Church, Genesis 2:19-25 and Ephesians 5:22-33, and that divorce and remarriage is only allowed by Christ in the case of unrepentant marital infidelity and abandonment by an unbeliever, Matthew 19:9 and 1 Corinthians 7:15.
+# 6. To order our relationships in the Biblical pattern of respect and submission outlined in Romans 13:1, Hebrews 13:17, and Ephesians 5:21-6:9 with citizens obeying governmental authorities, believers obeying Christian leaders, husbands loving wives, wives submitting to husbands, children obeying parents, fathers not exasperating, but bring children up in the Lord, servants obeying their masters, and masters caring for their servants.
+# 7. To be disciples of Christ, trusting and obeying God, loving God and one another, and calling others to do the same, Matthew 22:35-40 and Matthew 28:18-20.
+# 8. To know God and to make him known to the unbelieving, 1 Corinthians 15:1-7 and Philemon 6.
+# 9. To value, defend, and protect the sanctity of life, mankind being created by God in His image so that human life is of inestimable worth in all its dimensions, including pre-born babies, the aged, the physically or mentally challenged, and every other stage or condition from conception through natural death, Psalm 139:13-16 and Jeremiah 1:4-10.
+# 10. To live sober minded lives free from drunkenness, substance abuse, gluttony, or any excess in order to honor our bodies which are the temple of the Holy Spirit and be fit and prepared for serving the Lord, Galatians 5:16-26 and Titus 2:11-14.
+# 11. To serve as Christ’s peacemaking agents on earth waging war against Satan’s divisive lies, limiting ourselves to the weapons of God’s word, prayer, and a Christ-like attitude, 2 Corinthians 10:3-5, Ephesians 6:10-20, and 1 Peter 4:1.
+# 12. To hold one another accountable that professing Christians have a responsibility to pursue a godly lifestyle and Christ-like life that is to be enforced and disciplined as described in Matthew 18:15-20, 1 Corinthians 5: 1-13, 2 Thessalonians 3:6-15, and 1 Timothy 5:17-21.
+# 13. This statement of Christian behavior does not exhaust the extent of our practice. The Bible itself, as the inspired and infallible Word of God that speaks with final authority concerning truth, morality, and the proper conduct of mankind, is the sole and final authority of all of our behavior.  We also recognize that every matter great or small is within the realm of God’s loving concern, but prioritize our concerns to the greatest matters of Christian conscience first, Romans 11:36 and 2 Corinthians 10:3-5.  For purposes of The Corporation’s practice the Board of Directors is The Corporation’s final interpretive authority on the Bible’s meaning and application.
+#
+# TRANSLATE:
 # 1. Pray, recruit a committee of faithful Christians, and confirm the above basic Statement of Faith.
 # 2. Agree that translations are not inspired or infallible, though hopefully faithfully made, because only the original autographs of Scripture are God-breathed and inerrant.
 # 3. Translate and do not paraphrase, prioritizing the contexts of phrase, sentence, paragraph, book, author, testament, Bible, and history.
